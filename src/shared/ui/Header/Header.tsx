37845d221bc82e2cebc7b9fd78052a289d13d88d
지오현 <jihkyuoo@naver.com>
2024-07-17 03:16:46 +0900

    add: Header shared 컴포넌트 추가

---
+++ src/shared/ui/Header/Header.tsx
@@ -0,0 +1,35 @@
+import type { ReactNode } from '@tanstack/react-router';
+
+import IconArrowLeft from '@/assets/icons/icon-arrow-left.svg';
+
+interface Props {
+  title?: ReactNode;
+  left?: ReactNode;
+  extra?: ReactNode;
+  navOption?: {
+    type: 'back'; // close type 등 추가 가능
+    onClick?: () => void;
+  };
+}
+
+const NavRecord = {
+  back: (onClick?: () => void) => <IconArrowLeft onClick={onClick} />,
+  // close type 등 추가 가능...
+};
+
+export function Header({ title, left, extra, navOption }: Props) {
+  return (
+    <div className={'px-6 py-4'}>
+      <div className={'relative flex'}>
+        <div className={'absolute left-0 top-0 flex h-full items-center space-x-3'}>
+          <div className={'cursor-pointer'}>{navOption && NavRecord[navOption.type](navOption.onClick)}</div>
+          <div>{left}</div>
+        </div>
+
+        <div className={'flex min-h-6 w-full items-center justify-center font-semibold'}>{title}</div>
+
+        <div className={'absolute right-0 top-0 flex h-full items-center'}>{extra}</div>
+      </div>
+    </div>
+  );
+}
