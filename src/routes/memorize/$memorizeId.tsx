3fac30089af97303aa3d4fafd9097b3ce67e102f
지오현 <jihkyuoo@naver.com>
2024-07-17 10:37:58 +0900

    add: memorize id 경로 설정 - 잘못된 경로 redirect (기본 main-text) - url 경로에 따른 section tab 설정 - tab action 시 네비게이션

--- src/routes/memorize/$memorizeId.tsx
+++ src/routes/memorize/$memorizeId.tsx
@@ -1,34 +1,51 @@
-import { createFileRoute, Outlet, useRouter } from '@tanstack/react-router';
+import { createFileRoute, Outlet, redirect, useLocation, useRouter } from '@tanstack/react-router';
 import { useState } from 'react';
 
 import { Header } from '@/shared/ui/Header/Header';
 import { Tabs, type TabItemProps } from '@/shared/ui/Tabs';
 
-type SectionType = 'MAIN_TEXT' | 'RECORD_LIST';
+type SectionType = 'main-text' | 'record';
 
 export const Route = createFileRoute('/memorize/$memorizeId')({
   component: MemorizeDetail,
+  beforeLoad: (ctx) => {
+    const isCorrect = ctx.location.pathname.includes('main-text') || ctx.location.pathname.includes('record');
+    if(!isCorrect) {
+      throw redirect({
+        to: `/${Route.to}/${'main-text'}`
+      });
+    }
+  }
 });
 
 function MemorizeDetail() {
   const { history } = useRouter();
-  const [ section, setSection ] = useState<SectionType>('MAIN_TEXT');
+  const navigate = Route.useNavigate();
+  const location = useLocation();
+  const lastSegment = location.pathname.split('/').pop() as SectionType;
+
+  const [ section, setSection ] = useState<SectionType>(lastSegment);
 
   const items: TabItemProps<SectionType>[] = [
     {
-      value: 'MAIN_TEXT',
+      value: 'main-text',
       label: '본문 보기',
     },
     {
-      value: 'RECORD_LIST',
+      value: 'record',
       label: '녹음 목록',
     },
   ];
 
+  const handleChangeSection = (section: SectionType) => {
+    setSection(section);
+    navigate({ to: `/${Route.to}/${section}` });
+  };
+
   return (
     <>
       <Header title={'제목'} navOption={{ type: 'back', onClick: () => history.back() }} />
-      <Tabs value={section} items={items} onChange={e => setSection(e.target.value as SectionType)} />
+      <Tabs value={section} items={items} onChange={e => handleChangeSection(e.target.value as SectionType)} />
       <Outlet />
     </>
   );
